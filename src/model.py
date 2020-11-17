ad6db9a00163557e9a18d5b847448e17e62d2ed6
zschira <zasc3143@colorado.edu>
2020-11-16 22:05:03 -0500

    Testing stuff

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
-from keras import layers
-from keras import Model, Input
-import keras
-import keras.backend as K
+from tensorflow.keras import layers
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import Layer
+import tensorflow as tf
 
 class LeafNet():
     def __init__(self):
@@ -94,23 +94,25 @@ class LeafNet():
         hsi_input = Input(shape=(200, 200, 3), name="hsi")
         las_input = Input(shape=(40, 40, 70, 1), name="las")
         
-        roi_input = Input(shape=(625 * 9, 4), name="roi")
+        roi_input = Input(shape=(625 * 9, 4), name="roi_innie")
 
         image_net, las_net = self._conv_layers(chm_input, rgb_input, hsi_input, las_input)
         las_up = layers.UpSampling2D(size=7)(las_net)
         features = layers.Concatenate(axis=3)([las_up, image_net])
-        roi_pool = RoiPoolingConv(5, 625 * 9)([features, roi_input])
+        roi_pool = ROIPoolingLayer(5, 5)([features, roi_input])
         roi_pool = layers.Flatten()(roi_pool)
 
-        fc = layers.Dense(256)(roi_pool)
+        fc = layers.Dense(30)(roi_pool)
 
+        """
         labels = layers.Dense(30)(fc, activation="sigmoid")
         bounds = layers.Dense(120)(fc)
         bounds = layers.Reshape((30, 4))(bounds)
+        """
 
         self.detector = Model(
             inputs=[rgb_input, chm_input, hsi_input, las_input],
-            outputs=[bounds, labels],
+            outputs=[roi_pool],
         )
 
         self.detector.summary()
@@ -137,121 +139,107 @@ class LeafNet():
         return self.model.predict(data_sequence)
 
 
-class RoiPoolingConv(layers.Layer):
-    '''ROI pooling layer for 2D inputs.
-    See Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition,
-    K. He, X. Zhang, S. Ren, J. Sun
-    # Arguments
-        pool_size: int
-            Size of pooling region to use. pool_size = 7 will result in a 7x7 region.
-        num_rois: number of regions of interest to be used
-    # Input shape
-        list of two 4D tensors [X_img,X_roi] with shape:
-        X_img:
-        `(1, channels, rows, cols)` if dim_ordering='th'
-        or 4D tensor with shape:
-        `(1, rows, cols, channels)` if dim_ordering='tf'.
-        X_roi:
-        `(1,num_rois,4)` list of rois, with ordering (x,y,w,h)
-    # Output shape
-        3D tensor with shape:
-        `(1, num_rois, channels, pool_size, pool_size)`
-    '''
-    def __init__(self, pool_size, num_rois, **kwargs):
-
-        self.dim_ordering = 'tf'
-        assert self.dim_ordering in {'tf', 'th'}, 'dim_ordering must be in {tf, th}'
-
-        self.pool_size = pool_size
-        self.num_rois = num_rois
-
-        super(RoiPoolingConv, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        if self.dim_ordering == 'th':
-            self.nb_channels = input_shape[0][1]
-        elif self.dim_ordering == 'tf':
-            self.nb_channels = input_shape[0][3]
-
+class ROIPoolingLayer(Layer):
+    """ Implements Region Of Interest Max Pooling 
+        for channel-first images and relative bounding box coordinates
+        
+        # Constructor parameters
+            pooled_height, pooled_width (int) -- 
+              specify height and width of layer outputs
+        
+        Shape of inputs
+            [(batch_size, pooled_height, pooled_width, n_channels),
+             (batch_size, num_rois, 4)]
+           
+        Shape of output
+            (batch_size, num_rois, pooled_height, pooled_width, n_channels)
+    
+    """
+    def __init__(self, pooled_height, pooled_width, **kwargs):
+        self.pooled_height = pooled_height
+        self.pooled_width = pooled_width
+        
+        super(ROIPoolingLayer, self).__init__(**kwargs)
+        
     def compute_output_shape(self, input_shape):
-        if self.dim_ordering == 'th':
-            return None, self.num_rois, self.nb_channels, self.pool_size, self.pool_size
-        else:
-            return None, self.num_rois, self.pool_size, self.pool_size, self.nb_channels
+        """ Returns the shape of the ROI Layer output
+        """
+        feature_map_shape, rois_shape = input_shape
+        assert feature_map_shape[0] == rois_shape[0]
+        batch_size = feature_map_shape[0]
+        n_rois = rois_shape[1]
+        n_channels = feature_map_shape[3]
+        return (batch_size, n_rois, self.pooled_height, 
+                self.pooled_width, n_channels)
 
-    def call(self, x, mask=None):
+    def call(self, x):
+        """ Maps the input tensor of the ROI layer to its output
+        
+            # Parameters
+                x[0] -- Convolutional feature map tensor,
+                        shape (batch_size, pooled_height, pooled_width, n_channels)
+                x[1] -- Tensor of region of interests from candidate bounding boxes,
+                        shape (batch_size, num_rois, 4)
+                        Each region of interest is defined by four relative 
+                        coordinates (x_min, y_min, x_max, y_max) between 0 and 1
+            # Output
+                pooled_areas -- Tensor with the pooled region of interest, shape
+                    (batch_size, num_rois, pooled_height, pooled_width, n_channels)
+        """
+        def curried_pool_rois(x): 
+          return ROIPoolingLayer._pool_rois(x[0], x[1], 
+                                            self.pooled_height, 
+                                            self.pooled_width)
+        
+        pooled_areas = tf.map_fn(curried_pool_rois, x, dtype=tf.float32)
 
-        assert(len(x) == 2)
-
-        img = x[0]
-        rois = x[1]
-
-        input_shape = K.shape(img)
-
-        outputs = []
-
-        for roi_idx in range(self.num_rois):
-
-            x = rois[0, roi_idx, 0]
-            y = rois[0, roi_idx, 1]
-            w = rois[0, roi_idx, 2]
-            h = rois[0, roi_idx, 3]
-            
-            row_length = w / float(self.pool_size)
-            col_length = h / float(self.pool_size)
-
-            num_pool_regions = self.pool_size
-
-            #NOTE: the RoiPooling implementation differs between theano and tensorflow due to the lack of a resize op
-            # in theano. The theano implementation is much less efficient and leads to long compile times
-
-            if self.dim_ordering == 'th':
-                for jy in range(num_pool_regions):
-                    for ix in range(num_pool_regions):
-                        x1 = x + ix * row_length
-                        x2 = x1 + row_length
-                        y1 = y + jy * col_length
-                        y2 = y1 + col_length
-
-                        x1 = K.cast(x1, 'int32')
-                        x2 = K.cast(x2, 'int32')
-                        y1 = K.cast(y1, 'int32')
-                        y2 = K.cast(y2, 'int32')
-
-                        x2 = x1 + K.maximum(1,x2-x1)
-                        y2 = y1 + K.maximum(1,y2-y1)
-                        
-                        new_shape = [input_shape[0], input_shape[1],
-                                     y2 - y1, x2 - x1]
-
-                        x_crop = img[:, :, y1:y2, x1:x2]
-                        xm = K.reshape(x_crop, new_shape)
-                        pooled_val = K.max(xm, axis=(2, 3))
-                        outputs.append(pooled_val)
-
-            elif self.dim_ordering == 'tf':
-                x = K.cast(x, 'int32')
-                y = K.cast(y, 'int32')
-                w = K.cast(w, 'int32')
-                h = K.cast(h, 'int32')
-
-                rs = tf.image.resize(img[:, y:y+h, x:x+w, :], (self.pool_size, self.pool_size))
-                outputs.append(rs)
-
-        final_output = K.concatenate(outputs, axis=0)
-        final_output = K.reshape(final_output, (1, self.num_rois, self.pool_size, self.pool_size, self.nb_channels))
-
-        if self.dim_ordering == 'th':
-            final_output = K.permute_dimensions(final_output, (0, 1, 4, 2, 3))
-        else:
-            final_output = K.permute_dimensions(final_output, (0, 1, 2, 3, 4))
-
-        return final_output
+        return pooled_areas
     
+    @staticmethod
+    def _pool_rois(feature_map, rois, pooled_height, pooled_width):
+        """ Applies ROI pooling for a single image and varios ROIs
+        """
+        def curried_pool_roi(roi): 
+          return ROIPoolingLayer._pool_roi(feature_map, roi, 
+                                           pooled_height, pooled_width)
+        
+        pooled_areas = tf.map_fn(curried_pool_roi, rois, dtype=tf.float32)
+        return pooled_areas
     
-    def get_config(self):
-        config = {'pool_size': self.pool_size,
-                  'num_rois': self.num_rois}
-        base_config = super(RoiPoolingConv, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+    @staticmethod
+    def _pool_roi(feature_map, roi, pooled_height, pooled_width):
+        """ Applies ROI pooling to a single image and a single region of interest
+        """
 
+        # Compute the region of interest        
+        feature_map_height = int(feature_map.shape[0])
+        feature_map_width  = int(feature_map.shape[1])
+        
+        h_start = tf.cast(feature_map_height * roi[0], 'int32')
+        w_start = tf.cast(feature_map_width  * roi[1], 'int32')
+        h_end   = tf.cast(feature_map_height * roi[2], 'int32')
+        w_end   = tf.cast(feature_map_width  * roi[3], 'int32')
+        
+        region = feature_map[h_start:h_end, w_start:w_end, :]
+        
+        # Divide the region into non overlapping areas
+        region_height = h_end - h_start
+        region_width  = w_end - w_start
+        h_step = tf.cast( region_height / pooled_height, 'int32')
+        w_step = tf.cast( region_width  / pooled_width , 'int32')
+        
+        areas = [[(
+                    i*h_step, 
+                    j*w_step, 
+                    (i+1)*h_step if i+1 < pooled_height else region_height, 
+                    (j+1)*w_step if j+1 < pooled_width else region_width
+                   ) 
+                   for j in range(pooled_width)] 
+                  for i in range(pooled_height)]
+        
+        # take the maximum of each area and stack the result
+        def pool_area(x): 
+          return tf.math.reduce_max(region[x[0]:x[2], x[1]:x[3], :], axis=[0,1])
+        
+        pooled_features = tf.stack([[pool_area(x) for x in row] for row in areas])
+        return pooled_features
